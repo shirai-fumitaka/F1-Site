a216896d6f7c165f9019e89f2e68f4641d025b2e
shirai-fumitaka <shirai@lifematics.co.jp>
2023-12-08 08:09:07 +0900

    マイグレーションファイルの更新

--- f1site/f1homepage/models.py
+++ f1site/f1homepage/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
-class Qualifying (models.Model):
-    venue = models.CharField(max_length=100)
-    rank = models.IntegerField(default=1)
+class Driver(models.Model):
     driver_name = models.CharField(max_length=100)
+    age = models.IntegerField(default=1)
+    birthplace = models.CharField(max_length=100)
 
     def __str__(self):
-     return'<Qualifyinz:id=' + str(self.id) + ','+ \
-      self.venue+self.driver_name+'('+str(self.rank)+')>'
-# Create your models here.
+        return f'<Driver:id={self.id}, name={self.driver_name}, age={self.age}, birthplace={self.birthplace}>'
+
+class Qualifying(models.Model):
+    gp = models.CharField(max_length=100)
+    rank = models.IntegerField(default=1)
+    driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'<Qualifying:id={self.id}, gp={self.gp}, driver={self.driver.driver_name}, rank={self.rank}>'
+
