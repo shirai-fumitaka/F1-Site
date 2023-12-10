173087651886d157bc660e96dde5cd96f756ff06
shirai-fumitaka <shirai@lifematics.co.jp>
2023-12-11 08:58:18 +0900

    予選テーブルの編集

--- f1site/f1homepage/models.py
+++ f1site/f1homepage/models.py
@@ -4,15 +4,45 @@ class Driver(models.Model):
     driver_name = models.CharField(max_length=100)
     age = models.IntegerField(default=1)
     birthplace = models.CharField(max_length=100)
+    team = models.CharField(max_length=100)
 
     def __str__(self):
-        return f'<Driver:id={self.id}, name={self.driver_name}, age={self.age}, birthplace={self.birthplace}>'
+        return f'<Driver:id={self.id}, name={self.driver_name}, age={self.age}, birthplace={self.birthplace}, team={self.team}>'
 
 class Qualifying(models.Model):
-    gp = models.CharField(max_length=100)
+    GP_CHOICES = [
+        ('default', 'バーレーンGP'),
+        ('サウジアラビアGP', 'サウジアラビアGP'),
+        ('オーストラリアGP', 'オーストラリアGP'),
+        ('アゼルバイジャンGP', 'アゼルバイジャンGP'),
+        ('マイアミGP', 'マイアミGP'),
+        ('モナコGP', 'モナコGP'),
+        ('スペインGP', 'スペインGP'),
+        ('カナダGP', 'カナダGP'),
+        ('オーストリアGP', 'オーストリアGP'),
+        ('イギリスGP', 'イギリスGP'),
+        ('ハンガリーGP', 'ハンガリーGP'),
+        ('ベルギーGP', 'ベルギーGP'),
+        ('オランダGP', 'オランダGP'),
+        ('イタリアGP', 'イタリアGP'),
+        ('シンガポールGP', 'シンガポールGP'),
+        ('日本GP', '日本GP'),
+        ('アメリカGP', 'アメリカGP'),
+        ('メキシコシティGP', 'メキシコシティGP'),
+        ('シンガポールGP', 'シンガポールGP'),
+        ('日本GP', '日本GP'),
+        ('アメリカGP', 'アメリカGP'),
+        ('メキシコシティGP', 'メキシコシティGP'),
+        ('サンパウロGP', 'サンパウロGP'),
+        ('ラスベガスGP', 'ラスベガスGP'),
+        ('アブダビGP', 'アブダビGP'),
+        
+    ]
+    year = models.IntegerField(default=2023)
+    gp = models.CharField(max_length=100,choices=GP_CHOICES, default='default')
     rank = models.IntegerField(default=1)
     driver = models.ForeignKey(Driver, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f'<Qualifying:id={self.id}, gp={self.gp}, driver={self.driver.driver_name}, rank={self.rank}>'
+        return f'<Qualifying:id={self.id}, year={self.year},gp={self.gp}, driver={self.driver.driver_name}, rank={self.rank}>'
 
